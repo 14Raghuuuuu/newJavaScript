813de0ea593e5c1275374ee7d199ae19e878b043
14Raghuuuuu <166184663+14Raghuuuuu@users.noreply.github.com>
2024-09-21 11:08:18 +0000

    Datatype Conversion

---
+++ 01_variables/typeconversion.js
@@ -0,0 +1,16 @@
+let isLoggin = 0
+let booleanLoggedIn = Boolean(isLoggin);
+console.log(booleanLoggedIn);
+
+let score = "4ab"
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 or false => 0;
+// "" => false
