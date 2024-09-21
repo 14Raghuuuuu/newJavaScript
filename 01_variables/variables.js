75869e66047431e99e4dd0d49697394342604d3f
14Raghuuuuu <166184663+14Raghuuuuu@users.noreply.github.com>
2024-09-21 07:58:59 +0000

    container config done

---
+++ 01_variables/variables.js
@@ -0,0 +1,5 @@
+const accountId = "234245"
+let accountEmail = "ragu@gmail.com"
+var accountPassword = 12345
+accountCity = "Hyderabad"
+console.table([accountId, accountEmail, accountPassword, accountCity]);
