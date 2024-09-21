93042312ce3c34a0fb5f547703e85daa6e25e2b0
14Raghuuuuu <166184663+14Raghuuuuu@users.noreply.github.com>
2024-09-21 09:44:39 +0000

    container config done

---
+++ 01_variables/datatypes.js
@@ -0,0 +1,15 @@
+let name = "raghu"    
+let age = 19
+let isHePassed = true
+let year = null
+let yearOfPassed; 
+console.table([name,age,isHePassed,year,yearOfPassed]);
+
+// these are the dataTypes in new version of js;
+
+// string 
+// number 
+// boolean 
+// null 
+// undefined 
+
