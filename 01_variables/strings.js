0e591d239860336289790896b874909ac96a0d9c
14Raghuuuuu <166184663+14Raghuuuuu@users.noreply.github.com>
2024-09-23 06:16:10 +0000

    strings done

---
+++ 01_variables/strings.js
@@ -0,0 +1,27 @@
+let name = "Raghu";
+let age = 21;
+console.log(`my name is ${name} and my age ${age} `);
+
+let game = "raghuLingala";
+console.log(game.length);
+console.log(game[5])
+console.log(game.toUpperCase());
+console.log(game.charAt(2));
+console.log(game.indexOf('g'));
+console.log(game.substring(0,5));
+
+let myName = "   raghu    "
+console.log(myName);
+console.log(myName.length);
+console.log(myName.trim());
+
+
+let url = "http://raghuccbp.tech/raghu%20Lingla"
+console.log(url.replace('%20','-'));
+console.log(url.includes("kiran"));
+console.log(url.includes("raghu"));
+
+let my= "raghu-lingala-mogilipet";
+console.log(my.split('-'));
+
+
