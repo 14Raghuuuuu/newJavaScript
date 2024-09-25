47e514ef511d6d7a99c2a9d972d23127eb3bf2e1
14Raghuuuuu <166184663+14Raghuuuuu@users.noreply.github.com>
2024-09-25 18:17:41 +0000

    object_01 done

---
+++ 03_objects/objects.js
@@ -0,0 +1,12 @@
+ // literals 
+ const myUser = {
+    name : "Raghu",
+    email : "raghulingala@gmail.com",
+    village : "mogilipet",
+}
+
+console.log(myUser.village);
+console.log(myUser["village"]);
+
+myUser.name = "ajay"
+console.log(myUser);
