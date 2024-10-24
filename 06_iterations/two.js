aa2f815d20d21f1132fab6181b9d376fb7c39382
raghulingala <166184663+14Raghuuuuu@users.noreply.github.com>
2024-10-24 06:52:35 +0000

    while and do while done

---
+++ 06_iterations/two.js
@@ -0,0 +1,21 @@
+// while and do while loop 
+
+let i = 0
+while (i<=10) {
+    // console.log(`these are the numbers ${i}`)
+    i++
+}
+
+let myArray = ["raghu","kiran", "rohith"]
+let myArr = 0
+while (myArr<=myArray.length) {
+    // console.log(myArr)
+    myArr = myArr+1
+}
+// do while loop
+
+let score = 0 
+do{
+    console.log(score)
+    score++
+}while (score<=10)
